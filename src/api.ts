b109fca44cc5b48c26d35a1ec18cfa928c07d07f
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-09-22 13:50:45 +0300

    Get all patients function

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,13 @@
+import axios from "axios";
 import data from "../mock_insured_patients.json";
+import { API_URL_BASE } from "./constants";
+import { PatientT } from "../types";
+
+export const getAllPatients = async () => {
+    const response = await axios.get(`${API_URL_BASE}/somepatients`);
+
+    return response.data as PatientT[];
+};
 
 export const getInsured = (id: string) => {
     const insured = data.find((insured) => insured.insuranceNumber === id);
