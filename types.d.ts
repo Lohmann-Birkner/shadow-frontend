9228747c9caa2035d88fa6100742de25462c8012
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-09-22 12:23:40 +0300

    Change patient type

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,16 @@
-export type InsuredT = {
-    firstName: string;
-    lastName: string;
-    dateOfBirth: string;
-    sex: string;
-    zipcode: string;
-    insuranceNumber: string;
+export type PatientT = {
+    ins_id: string;
+    first_name: string;
+    last_name: string;
+    Membership_number: number;
+    Person_indicator: number;
+    Gender: string;
+    Date_of_birth: string;
+    ZIP_code: string;
+    Insured_person_number: string;
+    Entry_date: string;
+    Discharge_date: string;
+    Reason_for_leaving: string;
 };
 
 export type TaskT = {
