e716ff471ffdf5666429935ae2d8d900dde54b36
Xiaoyan Hou <hou@lohmann-birkner.de>
2023-09-25 17:06:32 +0200

    responsive design and translation problems fixed

--- src/components/search-patient.tsx
+++ src/components/search-patient.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormMessage,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Loader2, Search } from "lucide-react";
@@ -21,102 +21,126 @@ import * as z from "zod";
 import { Button } from "@/components/ui/button";
 import { searchInputs } from "../../types";
 import { PatientColumns } from "./ui/table/columns";
+import { useIntl } from "react-intl";
 
 interface Props {
-    setSearchParameters: (searchContent: searchInputs | null) => void;
-    isLoading: boolean;
+  setSearchParameters: (searchContent: searchInputs | null) => void;
+  isLoading: boolean;
 }
 
 const FormSchema = z.object({
-    catalog: z.string({
-        required_error: "Search catalog is required",
-    }),
-    searchQuery: z.string({
-        required_error: "Search term is required",
-    }),
+  catalog: z.string({
+    required_error: "Search catalog is required",
+  }),
+  searchQuery: z.string({
+    required_error: "Search term is required",
+  }),
 });
 
 export default function SearchPatient({
-    setSearchParameters,
-    isLoading,
+  setSearchParameters,
+  isLoading,
 }: Props) {
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            searchQuery: "",
-        },
-    });
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      searchQuery: "",
+    },
+  });
 
-    function onSubmit(data: z.infer<typeof FormSchema>) {
-        setSearchParameters(data);
-    }
+  const [seletedCatalog, setSeletedCatalog] = useState("11111");
+  const { formatMessage } = useIntl();
 
-    const PatientColumnsTyped = PatientColumns() as {
-        header: string;
-        accessorKey: string;
-    }[];
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    setSearchParameters(data);
+  }
 
-    return (
-        <div className="md:w-1/2">
-            <Form {...form}>
-                <form
-                    className="flex space-x-4"
-                    onSubmit={form.handleSubmit(onSubmit)}>
-                    <FormField
-                        control={form.control}
-                        name="catalog"
-                        render={({ field }) => (
-                            <FormItem>
-                                <Select
-                                    onValueChange={field.onChange}
-                                    defaultValue={field.value}>
-                                    <FormControl>
-                                        <SelectTrigger className="w-[220px]">
-                                            <SelectValue placeholder="Select Category" />
-                                        </SelectTrigger>
-                                    </FormControl>
-                                    <SelectContent>
-                                        {PatientColumnsTyped.map((column) => (
-                                            <SelectItem
-                                                value={column.accessorKey}
-                                                key={column.accessorKey}>
-                                                {column.header}
-                                            </SelectItem>
-                                        ))}
-                                    </SelectContent>
-                                </Select>
+  const PatientColumnsTyped = PatientColumns() as {
+    header: string;
+    accessorKey: string;
+  }[];
 
-                                <FormMessage />
-                            </FormItem>
-                        )}
+  console.log(form.control._formValues.catalog);
+  console.log(seletedCatalog);
+
+  return (
+    <div className="md:w-1/2">
+      <Form {...form}>
+        <form className="flex space-x-4" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="catalog"
+            render={({ field }) => (
+              <FormItem>
+                <Select
+                  onValueChange={(value) => {
+                    field.onChange(value);
+                    setSeletedCatalog(value);
+                  }}
+                  defaultValue={field.value}
+                >
+                  <FormControl>
+                    <SelectTrigger className="w-[220px] mb-2 mr-2">
+                      <SelectValue
+                      className="w-48 mb-2 mr-2 border-black"
+                        placeholder={formatMessage({ id: "Select_a_catalog" })}
+                      />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {PatientColumnsTyped.map((column) => (
+                      <SelectItem
+                        value={column.accessorKey}
+                        key={column.accessorKey}
+                      >
+                        {column.header}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="searchQuery"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  {seletedCatalog === "Date_of_birth" ||
+                  seletedCatalog === "Entry_date" ||
+                  seletedCatalog === "Discharge_date" ? (
+                    <Input {...field} type="date" className="w-48 mb-2 mr-2"
+                    icon={<Search className="mx-2 h-4 w-4" />}/>
+                  ) : (
+                    <Input
+                      {...field}
+                      placeholder="Suchen..."
+                      className="w-48 mb-2 mr-2"
+                      icon={<Search className="mx-2 h-4 w-4" />}
                     />
-                    <FormField
-                        control={form.control}
-                        name="searchQuery"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormControl>
-                                    <Input
-                                        {...field}
-                                        placeholder="Suchen..."
-                                        className="w-56"
-                                        icon={
-                                            <Search className="mx-2 h-4 w-4" />
-                                        }
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <Button disabled={isLoading} type="submit">
-                        {isLoading && (
-                            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                        )}
-                        {isLoading ? "Loading..." : "Search"}
-                    </Button>
-                </form>
-            </Form>
-        </div>
-    );
+                  )}
+                  {/* <Input
+                    {...field}
+                    placeholder="Suchen..."
+                    
+                    icon={<Search className="mx-2 h-4 w-4" />}
+                    type="date"
+                  /> */}
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button disabled={isLoading} type="submit">
+            {isLoading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            {isLoading ? "Loading..." : "Search"}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
 }
