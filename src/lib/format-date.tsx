59b890c6a7edfb3990750a57facf3544f5dfe230
Nadav Kirsh <saturdaysyouth@gmail.com>
2023-09-29 08:51:27 +0200

    Flip locale

--- src/lib/format-date.tsx
+++ src/lib/format-date.tsx
@@ -2,7 +2,7 @@ import { useIntl } from "react-intl";
 
 export const FormatDate = (date: string) => {
     const { locale } = useIntl();
-    if (locale === "en") {
+    if (locale === "de") {
         return date.split("-").reverse().join("-");
     } else {
         return date;
